c20e642162d93d49e197849eb3fde5ecbe1f0ebe
stan <hshl4314@gmail.com>
2019-07-20 11:20:36 +0800

    use usbserial

--- app/src/main/java/com/kt/usb/MainActivity.java
+++ app/src/main/java/com/kt/usb/MainActivity.java
@@ -5,18 +5,17 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.hardware.usb.UsbAccessory;
 import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
-import android.os.ParcelFileDescriptor;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import com.felhr.usbserial.UsbSerialDevice;
+import com.felhr.usbserial.UsbSerialInterface;
+
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -28,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private UsbManager manager;
     private PendingIntent permissionIntent;
     private UsbDevice targetDevice;
+    private UsbSerialDevice serial;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,10 +35,10 @@ public class MainActivity extends AppCompatActivity {
         permissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
         IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
         registerReceiver(usbReceiver,filter);
-        enum_devices(null);
+        enum_devices();
     }
 
-    public void enum_devices(View view) {
+    public void enum_devices() {
         manager = (UsbManager) getSystemService(Context.USB_SERVICE);
         HashMap<String, UsbDevice> deviceList = manager.getDeviceList();
         Iterator<UsbDevice> deviceIterator = deviceList.values().iterator();
@@ -50,26 +50,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-    UsbAccessory accessory;
-    ParcelFileDescriptor fileDescriptor;
-    FileInputStream inputStream;
-    FileOutputStream outputStream;
 
-    private void openAccessory() {
-        Log.d(TAG, "openAccessory: " + accessory);
-        fileDescriptor = manager.openAccessory(accessory);
-        if (fileDescriptor != null) {
-            FileDescriptor fd = fileDescriptor.getFileDescriptor();
-            inputStream = new FileInputStream(fd);
-            outputStream = new FileOutputStream(fd);
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-
-                }
-            }).start();
-        }
-    }
     private final BroadcastReceiver usbReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
@@ -80,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
                         if(device != null && device.getProductId() == pid && device.getVendorId() == vid){
                           //call method to set up device communication
                             Log.e(TAG, "enum_devices: find device!!!");
+                            targetDevice = device;
+                            initAsSerial();
                        }
                     } else {
                         Log.d(TAG, "permission denied for device " + device);
@@ -87,17 +70,50 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
             if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action)) {
-                UsbAccessory accessory = intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
-                if (accessory != null) {
-                    // call your method that cleans up and closes communication with the accessory
+                UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                if (device != null) {
+                    // call your method that cleans up and closes communication with the device
+                    Log.e(TAG, "onReceive:  device detached!!!" );
+                    if (device.getProductId() == pid && device.getVendorId() == vid) {
+                        serial.close();
+                        serial=null;
+                    }
                 }
             }
         }
     };
 
+    private void initAsSerial() {
+        if (targetDevice != null && UsbSerialDevice.isSupported(targetDevice)) {
+            Log.e(TAG, "=====> initAsSerial " );
+            UsbDeviceConnection usbDeviceConnection = manager.openDevice(targetDevice);
+            serial = UsbSerialDevice.createUsbSerialDevice(targetDevice,usbDeviceConnection);
+            serial.setPortName("COM1");
+            serial.open();
+            serial.setBaudRate(115200);
+            serial.setDataBits(UsbSerialInterface.DATA_BITS_8);
+            serial.setParity(UsbSerialInterface.PARITY_ODD);
+            serial.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
+            serial.read(mCallback);
+        }
+    }
+
+    private UsbSerialInterface.UsbReadCallback mCallback = arg0 -> {
+        // Code here :)
+        Log.e(TAG, "onReceivedData: "+HexSupport.toHexFromBytes(arg0));
+    };
+
     @Override
     protected void onDestroy() {
+        serial.close();
         super.onDestroy();
         unregisterReceiver(usbReceiver);
+    }
+
+    public void send(View view) {
+        if (serial != null) {
+            Log.e(TAG, "======> send: data" );
+            serial.write("ED08010FFB010F0400000000".getBytes());
+        }
     }
 }
